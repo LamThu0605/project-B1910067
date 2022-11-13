4d588cfc7d3082ab9bbb9237f7de5ac03366b18b
Lam Thu <thub1912986@student.ctu.edu.vn>
2022-11-13 19:29:15 +0700

    19h29 13/11/2022

--- B1910067/page/main.php
+++
@@ -1,8 +0,0 @@
-
-  <?php
-  include("./page/main/slideshow.php");
-  include("./page/main/sidebar.php");
-  include("./page/main/header.php");
-  include("./page/main/index.php");
-  include("./page/main/tintuc.php");
-  ?>
--- B1910067/page/tintuc.php
+++
@@ -1,9 +0,0 @@
-<main>
-    <div class='bg-blog'>
-        <?php
-        include('../admin/config/config.php');
-        include("./page/tintuc/danhmucbaiviet.php");
-        ?>
-    </div>
-   
-</main>
